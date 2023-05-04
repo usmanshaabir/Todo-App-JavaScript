06d335aaf11a59a4ec4c5519d7a700d32a062d85
Nauman Shabbir <mnaumanshabbir@gmail.com>
2023-05-04 18:26:45 +0500

    our create toastify error

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,17 @@
 // Current Date
 init()
-function init(){
-    let getdata=JSON.parse(window.localStorage.getItem("users")) || []
+function init() {
+    let getdata = JSON.parse(window.localStorage.getItem("users")) || []
+
+    if (!getdata.length) {
+        document.getElementById('outPut').innerHTML = `<h1 style=text-align:center;>HURRAY! Add a task button to add your task<h1>`;
+        return
+    }
+
     let tableStarting = `<table class="table">`
     let tableEnd = `</table>`
     let tableHead = ` <thead><tr><th scope="col">#</th> <th scope="col">Title</th><th scope="col">Location</th><th scope="col">Description</th><th scope="col">Actions</th></tr></thead>`;
-    let tBody='';
+    let tBody = '';
     for (i = 0; i < getdata.length; i++) {
         let DataStore = getdata[i]
         tBody += `<tbody><tr><th scope="row">${i + 1}</th><td>${DataStore.title}</td><td>${DataStore.location}</td>  <td>${DataStore.description}</td> <td><button type="button" class="btn btn-primary" onclick='etidData(event)' data-value="${DataStore.id}">Edit</button> <button type="button" class="btn btn-danger" data-value="${DataStore.id}" onclick='deleteData(event)'>Delete</button></td><td></td>`
@@ -19,23 +25,40 @@ let today = new Date();
 document.getElementById('Today').innerHTML = today
 
 
-function showToast(text) {
+function showToast() {
+
+}
+
+
+function showNotification(massage, type) {
+    let bgColor;
+    switch (type) {
+        case "success":
+            bgColor = "linear-gradient(to right ,#1D976C,#93F9B9)";
+            break
+        case "error":
+            bgColor = "linear-gradient(to right ,#93291e,#ed213a)";
+            break
+        default:
+            bgColor = "#000"
+            break
+    }
+
     Toastify({
-      text: text,
-      duration: 3000,
-      destination: "https://github.com/apvarun/toastify-js",
-      newWindow: true,
-      close: true,
-      gravity: "bottom", // `top` or `bottom`
-      position: "left", // `left`, `center` or `right`
-      stopOnFocus: true, // Prevents dismissing of toast on hover
-      style: {
-        background: "linear-gradient(to right, #A71D31 ,#3F0D12)",
-      },
-      onClick: function(){} // Callback after click
-    }).showToast(text);
-  }
-  
+        text: massage,
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: bgColor,
+        },
+        onClick: function () { } // Callback after click
+    }).showToast();
+}
 
 
 function getRandomId() {
@@ -63,15 +86,15 @@ function handleSubmit() {
     description = description.trim()
 
     if (title.length < 3) {
-        showToast("Plz Enter Your Full Name");
+        showNotification("Plz Enter Your Full Name", "error");
         return
     }
     if (location.length < 3) {
-        showToast("Plz Enter Your Location");
+        showNotification("Plz Enter Your Location", "error");
         return
     }
     if (description.length < 3) {
-        showToast("Plz Enter Your Description");
+        showNotification("Plz Enter Your Description", "error");
         return
     }
 
@@ -87,7 +110,7 @@ function handleSubmit() {
 
     localStorage.setItem('users', JSON.stringify(todos))
 
-    showToast("Your Data submited Success")
+    showNotification("Your Data submited Successfully", "success")
 
     emptyFlied()
     showData()
@@ -128,6 +151,8 @@ deleteData = (event) => {
         return user.id !== todoId
     })
     localStorage.setItem('users', JSON.stringify(afterDeletetodos))
+
+    showNotification("Deleted your Data", "error")
     showData()
 }
 
@@ -153,14 +178,18 @@ etidData = (event) => {
     document.getElementById('addTask').style.display = 'none';
     document.getElementById('updateTask').style.display = 'block';
 
-
 }
 
 var startTime = new Date();
 var timerElement = document.getElementById("Today");
 
 function updateTimer() {
-  var currentTime = new Date(); 
-  timerElement.innerHTML = currentTime; 
+    var currentTime = new Date();
+    timerElement.innerHTML = currentTime;
 }
 setInterval(updateTimer, 1000);
+
+
+
+
+
